571901f3cb77b5250c501f4c294d0ccfb931d77d
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-19 16:00:25 -0600

    hide vue env vars

--- challenges/challenges.client/src/env.js
+++
@@ -1,6 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
