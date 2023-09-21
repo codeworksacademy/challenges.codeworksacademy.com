57b53797557ee612dc1c9517c522039e023272fc
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-09-21 14:55:43 -0600

    added typescript to vite config

--- challenges/challenges.client/vite.config.js
+++ challenges/challenges.client/vite.config.js
@@ -1,9 +1,14 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),],
   build: {
     outDir: 'docs',
     sourcemap: false
@@ -12,3 +17,4 @@ export default defineConfig({
     port: 8080
   }
 })
+
