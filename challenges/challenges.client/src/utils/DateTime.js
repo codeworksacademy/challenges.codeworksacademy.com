38fb09a2b736e08050c44db7f642e7e22fc15150
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-29 11:30:53 -0600

    fixed my error in DateTime.js util

--- challenges/challenges.client/src/utils/DateTime.js
+++ challenges/challenges.client/src/utils/DateTime.js
@@ -1,24 +1,24 @@
-export default {
+export const FormatDateAndTime = {
   name: 'FormatDateAndTime',
   props: {
     date: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     formattedDate() {
       return new Date(this.date).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
-        day: 'numeric'
-      })
+        day: 'numeric',
+      });
     },
     formattedTime() {
       return new Date(this.date).toLocaleTimeString('en-US', {
         hour: 'numeric',
-        minute: 'numeric'
-      })
-    }
+        minute: 'numeric',
+      });
+    },
   }
 }
