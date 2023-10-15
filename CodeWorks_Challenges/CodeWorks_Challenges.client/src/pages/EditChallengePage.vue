1c96addfbdc2b5e6ba09cf702b22300b456e6e63
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-10-15 15:23:39 -0600

    put works

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/EditChallengePage.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/EditChallengePage.vue
@@ -19,27 +19,28 @@
   this.participantCount = data.participantCount # shown, thumbnails of first 5 users followed by "+ participantCount others"  
   merge test--> 
 <template>
-  <article class="text-dark">
-    <section class="container-fluid">
+  <div class="">
+    <section class="container-fluid text-dark">
       <h1 v-if="!challenge.valid" class="text-warning">Challenge is invalid</h1>
-      <form @submit.prevent="updateChallenge"></form>
-      <label for="name">Challenge Name</label>
-      <input type="text" class="form-control" id="name" v-model="editable.name" required> 
-      <label for="description">Challenge Description</label>
-      <textarea type="text" class="form-control" id="description" v-model="editable.description" required></textarea> 
-      <div>
-        <section>
-          <h1 v-if="challenge.steps.length == 0" class="">This challenge has zero steps</h1>
-          <label for="steps">Challenge Steps</label>
-          <h4>Add a step</h4>
-          <textarea name="" id="stepText" cols="30" rows="10"></textarea>
-          <i class="mdi mdi-plus-box fs-1" @click="addStep"></i>
-        </section>
-        <section class="container-fluid" v-for="(step, index) in challenge.steps">
-          <h1>{{ index + 1 }} <i class="mdi mdi-trash-can" @click="deleteStep(index)"></i></h1>
-          <textarea name="" id="" cols="30" rows="10">{{ step }}</textarea>
-        </section>
-      </div>
+      <form @submit.prevent="updateChallenge">
+        <label for="name">Challenge Name</label>
+        <input type="text" class="form-control" id="name" v-model="editable.name" required> 
+        <label for="description">Challenge Description</label>
+        <textarea type="text" class="form-control" id="description" v-model="editable.description" required></textarea> 
+          <div>
+            <section>
+              <h1 v-if="challenge.steps.length == 0" class="">This challenge has zero steps</h1>
+              <label for="steps">Challenge Steps</label>
+              <h4>Add a step</h4>
+              <textarea name="" id="stepText" cols="30" rows="10"></textarea>
+              <i class="mdi mdi-plus-box fs-1" @click="addStep"></i>
+            </section>
+            <section class="container-fluid" v-for="(step, index) in challenge.steps">
+              <h1>{{ index + 1 }} <i class="mdi mdi-trash-can" @click="deleteStep(index)"></i></h1>
+              <textarea name="" id="" cols="30" rows="10">{{ step }}</textarea>
+            </section>
+          </div>
+      </form>
       <input type="text" class="form-control" id="coverImg" v-model="editable.coverImage" required> 
       <!-- //REPLACE Textarea with Step thing -->
       <textarea name="supportLinks" id="" cols="30" rows="10">Support Links</textarea>
@@ -52,10 +53,10 @@
       <button @click="editChallenge()" class="btn btn-warning" v-if="editing">Cancel Edit</button>
       <button @click="updateChallenge()" class="btn btn-success" v-if="editing">Update Challenge</button>
     </section>
-    <form v-for="(value, key) in challenge" :key="key" id="challengeEditForm" v-if="editing" class="form-box">
-      <div class="d-flex justify-content-between">
+    <form v-for="(value, key) in challenge" :key="key" id="challengeEditForm" v-if="editing" class="">
+      <div class="d-flex justify-content-between text-dark">
         <h1 class="">{{ key }}</h1>
-        <input type="text" :value="value" class="form-control w-50">
+        <input type="text" :value="value" class="form-control w-50 input-box">
       </div>
     </form>
     <section>
@@ -67,11 +68,11 @@
       <h1>{{ index + 1 }} <i class="mdi mdi-trash-can" @click="deleteStep(index)"></i></h1>
       <textarea name="" id="" cols="30" rows="10">{{ step }}</textarea>
     </section>
-  </article>
+  </div>
 </template>
 
 <script>
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref , watchEffect} from 'vue'
 import { AppState } from '../AppState'
 import Pop from "../utils/Pop.js"
 import { logger } from "../utils/Logger.js"
@@ -87,7 +88,10 @@ export default {
 
     let editing = ref(false);
     const editable = ref({})
-
+    watchEffect(() => {
+      editable.value = AppState.activeChallenge
+    })
+    
     async function setActiveChallenge() {
       try {
         await challengesService.setActiveChallenge(AppState.activeChallenge?.id)
@@ -99,7 +103,7 @@ export default {
 
     async function updateChallenge() {
       try {
-        await challengesService.updateChallenge(AppState.activeChallenge)
+        await challengesService.updateChallenge(AppState.activeChallenge, AppState.activeChallenge.id)
         Pop.toast('Challenge Updated')
       } catch (error) {
         Pop.toast(error.message, 'error')
