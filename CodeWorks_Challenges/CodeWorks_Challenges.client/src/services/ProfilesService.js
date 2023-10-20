f8991c07cca76bec198bf5bf23bf4ef7c531ecc7
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2023-10-20 10:45:14 -0600

    Updated Profile search, Returns appropriately

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/services/ProfilesService.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/services/ProfilesService.js
@@ -5,7 +5,7 @@ import { api } from "./AxiosService"
 
 class ProfilesService {
   async getProfiles(name) {
-    const res = await api.get(`api/profiles/?query=${name}`)
+    const res = await api.get(`api/profiles/?name=${name}`)
     logger.log('[RETRIEVED PROFILES]', res.data)
     AppState.profiles = res.data.map(p => new Profile(p))
   }
