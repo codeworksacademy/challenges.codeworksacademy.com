ebe453327802f670c0bb3adc87f6c6977d51155e
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-10-22 04:50:09 -0600

    badges for founders almost complete

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/Rewards/Badges/CustomBadge.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/Rewards/Badges/CustomBadge.vue
@@ -2,15 +2,34 @@
   <section class="container-fluid position-relative p-3">
     <div class="background-box m-auto">
       <div class="img-box m-auto">
-        <span class="top-ribbon"></span>
         <img src="../../../assets/img/ultimate-hacker.png" alt="Ultimate Hacker Badge Image">
         <img src="../../../assets/img/gold-ribbon.png" alt="Small Gold Ribbon Image" class="gold-ribbon">
-        <span class="bottom-ribbon"></span>
+        <svg viewBox="0 0 500 500">
+          <path
+            id="curve"
+            d="M0 140 C200 300 300 50 500 150"
+            fill="transparent"
+            stroke="darkgoldenrod"
+            stroke-width="50"
+          />
+          <text width="500">
+            <textPath
+              xlink:href="#curve"
+              startOffset="50%"
+              text-anchor="middle"
+              fill="black"
+              stroke="black"
+            >
+              Founder of 'App Name'
+            </textPath>
+          </text>
+        </svg>
       </div>
     </div>
   </section>
-</template>
   
+</template>
+
 <script>
 
 export default {
@@ -33,7 +52,7 @@ export default {
   width: 300px;
   min-height: 350px;
   background-image: url(../../../assets/img/bg-custom-badge.png);
-  background-size: cover;
+  background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
   filter: drop-shadow(2px 1px 5px #222222);
@@ -76,37 +95,37 @@ export default {
       bottom: 0;
       background-clip: border-box;
       background: conic-gradient(
-        #222222 0% 2.5%,
+        transparent 0% 2.5%,
         var(--primary-green) 2.5% 7.5%,
-        #222222 7.5% 10%,
+        transparent 7.5% 10%,
         var(--primary-blue) 10% 15%,
-        #222222 15% 17.5%,
+        transparent 15% 17.5%,
         var(--secondary-magenta) 17.5% 22.5%,
-        #222222 22.5% 25%,
+        transparent 22.5% 25%,
         var(--tertiary-orange) 25% 30%,
-        #222222 30% 32.5%,
+        transparent 30% 32.5%,
         var(--primary-green) 32.5% 37.5%,
-        #222222 37.5% 40%,
+        transparent 37.5% 40%,
         var(--primary-blue) 40% 45%,
-        #222222 45% 47.5%,
+        transparent 45% 47.5%,
         var(--secondary-magenta) 47.5% 52.5%,
-        #222222 52.5% 55%,
+        transparent 52.5% 55%,
         var(--tertiary-orange) 55% 60%,
-        #222222 60% 62.5%,
+        transparent 60% 62.5%,
         var(--primary-green) 62.5% 67.5%,
-        #222222 67.5% 70%,
+        transparent 67.5% 70%,
         var(--primary-blue) 70% 75%,
-        #222222 75% 77.5%,
+        transparent 75% 77.5%,
         var(--secondary-magenta) 77.5% 82.5%,
-        #222222 82.5% 85%,
+        transparent 82.5% 85%,
         var(--tertiary-orange) 85% 90%,
-        #222222 90% 92.5%,
+        transparent 90% 92.5%,
         var(--primary-green) 92.5% 97.5%,
-        #222222 97.5% 100%,
+        transparent 97.5% 100%,
         );
       clip-path: path('M100 0 L200 35 Q190 175 100 200 Q10 175 0 35 L100 0');
       transition: .5s;
-      z-index: 0;
+      z-index: 2;
       transform: scale(1.1);
       filter: brightness(.8);
     }
@@ -124,11 +143,14 @@ export default {
       left: 0;
       right: 0;
       bottom: 0;
+      width: 100%;
+      height: 100%;
       clip-path: path('M100 0 L200 35 Q190 175 100 200 Q10 175 0 35 L100 0');
       transition: .5s;
-      transform: scale(1);
-      opacity: .25;
+      transform: scale(1.1);
+      opacity: .7;
       z-index: 1;
+      filter: brightness(.8) saturate(1.2) grayscale(.75);
     }
     img {
       position: relative;
@@ -180,6 +202,31 @@ export default {
       transform: scale(1.1);
       z-index: 1000;
       background-color: goldenrod;
+    }
+  }
+  svg {
+    position: relative;
+    left: 0;
+    right: 0;
+    bottom: 14rem;
+    width: 200px;
+    height: 200px;
+    transition: .5s;
+    transform: scale(1.1);
+    z-index: 3;
+    fill: transparent;
+    stroke: rgb(144, 106, 9);
+
+    text {
+      font-family: 'Lobster';
+      stroke-width: 1px;
+      font-size: 2.2rem;
+      font-weight: 550;
+      text-align: center;
+      text-transform: uppercase;
+      transform: translateY(.75rem);
+      text-shadow: 0 .5px .5px #fff;
+      letter-spacing: 0px;
     }
   }
 }
