d2bc0a48ec27036d49ac28e8b8e260771179e0f5
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-10-25 23:29:11 -0600

    some cleanup of my own code

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/Participant.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/Participant.js
@@ -8,7 +8,6 @@ export class Participant {
     this.challenge = data.challenge ? new Challenge(data.challenge) : null
     //FIXME - JAKE - Just wanted to be sure to wait and remove the status property until we are sure we don't need it after you reference the schema in the server for a Participant. Thanks! - AJ
     // this.status = data.status || 'inactive'
-    this.hasAuth = data.hasAuth || false
     this.profile = data.profile
     this.createdAt = data.createdAt
     this.updatedAt = data.updatedAt
--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/Reward.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/Reward.js
@@ -6,13 +6,12 @@ export class Reward {
     this.challengeId = data.challengeId
     this.accountId = data.accountId
     this.name = data.name
-    this.badges = data.badges
-    this.points = data.points
-    this.experience = data.experience
+    // this.badges = data.badges
+    // this.points = data.points
+    // this.experience = data.experience
     this.claimed = data.claimed
-    this.claimedBy = data.claimedBy
+    // this.claimedBy = data.claimedBy
     this.claimedAt = data.claimedAt
-    this.participantId = data.participantId
     this.challenge = data.challenge ? new Challenge(data.challenge) : null
     this.account = data.account ? new Account(data.account) : null
   }
