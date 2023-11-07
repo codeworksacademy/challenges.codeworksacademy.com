5e3777beb81e31a5caa55876f8ae9fd0f1ec5a7e
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-11-07 12:18:50 -0700

    Update env.js

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/env.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/env.js
@@ -1,5 +1,11 @@
 export const dev = window.location.origin.includes('localhost')
+<<<<<<< Updated upstream
 export const baseURL = dev ? 'http://localhost:3000' : ''
+=======
+export const baseURL = dev ? 'http://localhost:3030' : ''
+export const apiURL = dev ? 'https://api.github.com' : ''
+export const apiKey = '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'
+>>>>>>> Stashed changes
 export const useSockets = false
 export const domain = 'dev-82py0l5jt61zn10o.us.auth0.com'
 export const clientId = 'hykoUyNDCrsovYQXXUKObg3y1Iw4UhUS'
