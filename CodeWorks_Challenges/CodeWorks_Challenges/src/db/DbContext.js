8d4316f7faad3a8c2119e352f6e3bc89392cc611
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2023-11-08 10:14:06 -0700

    re imported milestone, accountmilestone schemas

--- CodeWorks_Challenges/CodeWorks_Challenges/src/db/DbContext.js
+++ CodeWorks_Challenges/CodeWorks_Challenges/src/db/DbContext.js
@@ -5,6 +5,8 @@ import { ChallengeParticipantSchema } from '../models/Participant';
 import { ModeratorSchema } from "../models/Moderator.js";
 // import { AnswerSchema } from '../models/Answer.js';
 import { SubmissionSchema } from '../models/Submission.js';
+import { AccountMilestoneSchema } from "../models/AccountMilestone.js";
+import { MilestoneSchema } from "../models/Milestone.js";
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
@@ -13,6 +15,10 @@ class DbContext {
   Moderators = mongoose.model('Moderator', ModeratorSchema);
   // Answers = mongoose.model('Answer', AnswerSchema);
   Submissions = mongoose.model('Submission', SubmissionSchema);
+
+  // STUB Kyle - Milestones and AccountMilestones were removed and had to be re-imported. If these were removed intentionally please let me know why.
+  Milestones = mongoose.model('Milestone', MilestoneSchema);
+  AccountMilestones = mongoose.model('AccountMilestone', AccountMilestoneSchema);
 }
 
 export const dbContext = new DbContext()
