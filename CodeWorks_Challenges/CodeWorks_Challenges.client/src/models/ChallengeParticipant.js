43649ac380beb4d15ed5c8faa1dcad670ec889eb
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-11-17 14:47:30 -0700

    added requirements to challengeparticipant

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/ChallengeParticipant.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/models/ChallengeParticipant.js
@@ -11,6 +11,7 @@ export class ChallengeParticipant {
     this.profile = data.profile
     this.createdAt = data.createdAt
     this.updatedAt = data.updatedAt
+    this.requirements = data.requirements
     // this.pointValue = data.challenge ? new Challenge(data.challenge).pointValue : 0
   }
 }
