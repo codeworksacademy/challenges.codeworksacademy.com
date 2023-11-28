2283514450aa8b5344da99b276bc8a33ebf59cbb
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-11-28 14:53:42 -0700

    added accordion

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/GradeSubmissionPage.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/GradeSubmissionPage.vue
@@ -40,9 +40,35 @@
   <!-- <div v-for="p in participants" :key="p.id" class="col-12 d-flex justify-content-center align-items-center pb-5 mb-5">
     <GradeSubmissionForm v-if="p.status === 'submitted'" :participant="p" />
   </div> -->
-  <div v-for="p in participants" :key="p.id" class="col-12 d-flex justify-content-center align-items-center pb-5 mb-5">
+  <!-- <div v-for="p in participants" :key="p.id" class="col-12 d-flex justify-content-center align-items-center pb-5 mb-5">
     <div v-if="p.status === 'submitted' && challengeCreator">
       <GradeSubmissionForm :participant="p" />
+    </div>
+  </div> -->
+  <div class="accordion accordion-flush" id="accordionFlushExample">
+    <!-- <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+          Accordion Item #2
+        </button>
+      </h2>
+      <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+      </div>
+    </div> -->
+    <div v-for="p in participants" :key="p.id" class="accordion-item">
+      <div v-if="p.status === 'submitted' && challengeCreator">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse'+ p.id" aria-expanded="false" :aria-controls="'collapse' + p.id">
+              {{ p.profile.name }}
+            </button>
+        </h2>
+        <div :id="'collapse' + p.id" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <GradeSubmissionForm :participant="p" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   </section>
