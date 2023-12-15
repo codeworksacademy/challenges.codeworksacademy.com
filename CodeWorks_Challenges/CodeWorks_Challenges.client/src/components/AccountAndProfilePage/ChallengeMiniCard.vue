a5432c7e34d29bc497a95cfaab7c8c2b3e8c311c
Rebecca VandeWater <123334409+RebeccaVandeWater@users.noreply.github.com>
2023-12-14 18:14:56 -0700

    switching to dev

---
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/AccountAndProfilePage/ChallengeMiniCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="border-bottom border-dark d-flex text-light justify-content-between">
+    <div class="d-flex">
+      <p>
+        {{ challengeProp.name }}
+      </p>
+      <span>
+        {{ challengeProp.difficulty }}
+      </span>
+    </div>
+    <div>
+      {{ challengeProp.category || "Full Stack" }}
+    </div>
+    <div>
+      <router-link :to="{name: 'ChallengeDetails', params: { challengeId: challengeProp.id }}">
+        <button class="btn text-light">
+          <i class="mdi mdi-chevron-right"></i>
+        </button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { Challenge } from '../../models/Challenge';
+
+export default {
+  props:{
+    challengeProp: {type: Challenge, required: true}
+  },
+
+  setup(props){
+    return {
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/AccountChallenges.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/AccountChallenges.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid">
     <section class="row">
-      <div class="col-12">
-        
+      <div class="col-12" v-for="challenge in challenges">
+        <ChallengeMiniCard :challengeProp="challenge" />
       </div>
     </section>
   </div>
@@ -10,10 +10,17 @@
 
 
 <script>
+import { computed } from 'vue';
+import { AppState } from '../AppState';
+import ChallengeMiniCard from '../components/AccountAndProfilePage/ChallengeMiniCard.vue';
+
 export default {
-  setup(){
-    return {}
-  }
+    setup() {
+        return {
+            challenges: computed(() => AppState.myChallenges)
+        };
+    },
+    components: { ChallengeMiniCard }
 }
 </script>
 
