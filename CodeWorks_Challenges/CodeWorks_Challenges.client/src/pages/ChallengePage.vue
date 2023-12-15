c0178a7ab30fd32396ba70a5ab22a8d489ef0c71
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-12-14 18:22:52 -0700

    create challenge to be cleaned and refactored

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/ChallengePage.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/ChallengePage.vue
@@ -2,8 +2,8 @@
   <section class="container-fluid pt-5 g-0">
       <div class="col-12 d-flex justify-content-end">
         <div class="col-4 d-flex create-challenge-card flex-column" style="position: relative; height: 14vh;right: 5rem">
-          <span class="submission-subtitle" style="">Earn your Reputation</span>
-          <a ref="challenge" id="createChallengeButton" type="button" role="button" data-bs-target="#createChallengeForm" data-bs-toggle="modal" aria-label="Go to Active Challenge Modal" title="Create a new challenge" style="">Create a Challenge</a>
+          <span class="submission-subtitle text-uppercase" style="">Earn your Reputation</span>
+          <a ref="challenge" id="createChallengeButton" class="create-challenge" type="button" role="button" data-bs-target="#createChallengeForm" data-bs-toggle="modal" aria-label="Go to Active Challenge Modal" title="Create a new challenge" style="">Create a Challenge</a>
         </div>
       </div>
       
@@ -191,15 +191,16 @@ export default {
     transition: all .3s ease;
     .submission-subtitle {
       color: var(--shadow-green);
-      font-size: 1.2rem;
       font-weight: 500;
       margin-bottom: 1rem;
-    }
-    &:hover {
-      outline: 3px solid var(--border-dark);
-      color: var(--text-main);
-      transition: all .3s ease;
-      filter: brightness(1.1);
+      }
+      &>a {
+        text-decoration: none;
+        color: var(--shadow-blue);
+        &:hover {
+          filter: brightness(1.2);
+          transition: all .3s ease;
+      }
     }
   }
   .search-input {
