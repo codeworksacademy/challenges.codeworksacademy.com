6c57cb668b2a07c96c7d46d2e147c736d5c59bdb
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-12-17 07:41:06 -0700

    JSON & constants injected successfully

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/constants/index.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/constants/index.js
@@ -3,3 +3,5 @@ import * as data from '../../../CodeWorks_Challenges.shared/constants'
 export const SUBMISSION_TYPES = data.SUBMISSION_TYPES
 export const CATEGORY_TYPES = data.CATEGORY_TYPES
 export const STATUS_TYPES = data.STATUS_TYPES
+export const RANK_TYPES = data.RANK_TYPES
+export const BADGE_TYPES = data.BADGE_TYPES
--- CodeWorks_Challenges/CodeWorks_Challenges.shared/constants/index.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.shared/constants/index.js
@@ -3,4 +3,6 @@ import * as data from './data.json'
 export const SUBMISSION_TYPES = data.SUBMISSION_TYPES
 export const CATEGORY_TYPES = data.CATEGORY_TYPES
 export const STATUS_TYPES = data.STATUS_TYPES
+export const RANK_TYPES = data.RANK_TYPES
+export const BADGE_TYPES = data.BADGE_TYPES
 export const PROFILE_FIELDS = data.PROFILE_FIELDS
