819a8dbd2f95131854ce260cc1a879fdcbdba370
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-01-04 09:20:43 -0700

    ChallengeDetailsOverviewPage: ActiveChallengeReputationCard updated

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengeDetailsPage/ActiveChallengeReputationCard.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengeDetailsPage/ActiveChallengeReputationCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="details-card text-capitalize">
-    <i class="mdi mdi-account-star-outline fs-1"></i>
-    <h3>+{{ challenge.reputationIds.length }} Rep</h3>
-    <h6 class="text-center">Total Reputation</h6>
+    <i class="mdi mdi-account-star-outline"></i>
+    <h3 class="circle-container"><span class="circle">+{{ challenge.reputationIds.length }}</span></h3>
+    <h6 class="text-center pb-2">Total Reputation</h6>
   </div>
 </template>
 
@@ -17,3 +17,48 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.mdi-account-star-outline {
+  position: relative;
+  top: 10%;
+  font-size: 3rem;
+  transform: translateX(-10%);
+}
+.circle-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  .circle {
+    position: absolute;
+    top: 0;
+    right: 25%;
+    transform: translateY(-150%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    box-shadow: inset 0 0 0 2px #2a8f4c;
+    border-radius: 50%;
+    background-color: #38BB64;
+    color: white;
+    font-size: 1.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 1200px) {
+      right: 15%;
+    }
+    @media screen and (max-width: 992px) {
+      right: 5%;
+    }
+    @media screen and (max-width: 768px) {
+      right: 40%;
+    }
+  }
+}
+</style>
