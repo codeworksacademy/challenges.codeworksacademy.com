f186dd1dcf27ff6dd31f856adc5f4d3edfbfb385
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-01-07 22:13:39 -0700

    badgeImg

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/EditChallenge/EditChallengeBadge.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/EditChallenge/EditChallengeBadge.vue
@@ -18,7 +18,7 @@
             d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <div class="text-uppercase rounded-circle bg-light">
-          <img :src="challenge.badgeImg" alt="" class="img-fluid position-absolute" style="height: 50px; top: 25%; left: 50%; transform: translateX(-50%)">
+          <img :src="challenge.badgeImg" alt="" class="img-fluid position-absolute" style="height: 65px; top: 20%; left: 50%; transform: translateX(-50%)">
         </div>
       </div>
     </div>
