53a787344b074fd21bfc459e03cbca4e7c8d3af0
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-01-15 13:46:41 -0700

    Conditionally render reputation button

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengePage/ChallengeCreatorCard.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengePage/ChallengeCreatorCard.vue
@@ -1,15 +1,24 @@
 <template>
-  <div :class="`creator-details-card ${themeStyle ? 'theme-style' : ''} rounded text-capitalize p-2`" :style="{backgroundColor: bgColor, borderColor: color, borderStyle: 'groove'}">
+  <div :class="`creator-details-card ${themeStyle ? 'theme-style' : ''} rounded text-capitalize p-2`"
+    :style="{ backgroundColor: bgColor, borderColor: color, borderStyle: 'groove' }">
     <div class="col-4 d-flex flex-column justify-content-evenly align-items-center">
       <router-link :to="{ name: 'Profile.overview', params: { profileId: challenge.creator.id } }">
-        <img :src="challenge.creator.picture" :alt="`Image for Challenge creator named '${challenge.creator.name}' is broken`" :title="`Image of the Challenge Creator; ${challenge.creator.name}`" class="img-fluid rounded-circle me-2" style="height: 75px;width:75px">
+        <img :src="challenge.creator.picture"
+          :alt="`Image for Challenge creator named '${challenge.creator.name}' is broken`"
+          :title="`Image of the Challenge Creator; ${challenge.creator.name}`" class="img-fluid rounded-circle me-2"
+          style="height: 75px;width:75px">
       </router-link>
       <h5 class="text-uppercase pe-2">Creator</h5>
     </div>
     <div class="col-8 d-flex flex-column justify-content-evenly">
-      <h3 :style="isMobile ? 'padding-left: 4rem;' : 'padding-left: 1.5rem;'" class="col-12 creator-text d-flex justify-content-center offset-1 text-capitalize"> {{ challenge.creator.name }} </h3>
+      <h3 :style="isMobile ? 'padding-left: 4rem;' : 'padding-left: 1.5rem;'"
+        class="col-12 creator-text d-flex justify-content-center offset-1 text-capitalize"> {{ challenge.creator.name }}
+      </h3>
       <div class="col-12 button-container d-flex justify-content-center offset-2 mt-2">
-        <button @click="giveReputation" class="btn bg-dark btn-success text-success me-3"><small>Give Reputation</small></button>
+        <button v-if="reputant" @click="giveReputation" class="btn bg-dark btn-success text-success me-3"><small>Give
+            Reputation</small></button>
+        <button v-else @click="giveReputation" class="btn bg-dark btn-success text-info me-3"><small>Remove
+            Reputation</small></button>
       </div>
     </div>
   </div>
@@ -26,12 +35,12 @@ import { challengesService } from '../../services/ChallengesService'
 export default {
   props: {
     challenge: {
-      type: Challenge|| Object,
+      type: Challenge || Object,
       required: true
     },
-    bgColor: {type: String, required: true},
-    color: {type: String, required: true},
-    themeStyle: {type: Boolean, required: true, default: false}
+    bgColor: { type: String, required: true },
+    color: { type: String, required: true },
+    themeStyle: { type: Boolean, required: true, default: false }
   },
   setup(props) {
     const route = useRoute()
@@ -44,31 +53,37 @@ export default {
 
     const gaveReputation = computed(() => {
       const challenge = props.challenge
-      if(challenge.reputationIds.find(r => r == AppState.AccountState.account.id)){
+      if (challenge.reputationIds.find(r => r == AppState.AccountState.account.id)) {
         return true
       }
       return false
     })
 
-    async function giveReputation(){
-        try {
-          await challengesService.giveReputation(route.params.challengeId)
-          if(gaveReputation.value){
-            Pop.toast(`The CodeWorks team and ${props.challenge.creator.name} appreciate you gifting +1 Reputation to challenge '${props.challenge.name}'!`, 'success')
-          } else {
-            Pop.toast(`You have removed your reputation point to ${props.challenge.creator.name} for '${props.challenge.name}.'`, 'success')
-          }
-        } catch (error) {
-          Pop.error(error.message)
+    async function giveReputation() {
+      try {
+        await challengesService.giveReputation(route.params.challengeId)
+        if (gaveReputation.value) {
+          Pop.toast(`The CodeWorks team and ${props.challenge.creator.name} appreciate you gifting +1 Reputation to challenge '${props.challenge.name}'!`, 'success')
+        } else {
+          Pop.toast(`You have removed your reputation point to ${props.challenge.creator.name} for '${props.challenge.name}.'`, 'success')
         }
+      } catch (error) {
+        Pop.error(error.message)
       }
+    }
 
     return {
       route,
       isMobile,
       isParticipant,
       gaveReputation,
-      giveReputation
+      giveReputation,
+      reputant: computed(() => {
+        if (challenge.reputationIds.find(r => r == AppState.AccountState.account.id)) {
+          return true
+        }
+        return false
+      })
     }
   }
 }
