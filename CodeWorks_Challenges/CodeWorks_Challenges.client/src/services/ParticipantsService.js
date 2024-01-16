ac6ecea73e6524b516b6f52b09d153ef00e06350
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-01-16 15:46:27 -0700

    breaking things

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/services/ParticipantsService.js
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/services/ParticipantsService.js
@@ -2,7 +2,6 @@ import { api } from './AxiosService'
 import { AppState } from "../AppState"
 import { ChallengeParticipant } from "../models/ChallengeParticipant.js"
 import { logger } from './../utils/Logger';
-import Pop from '../utils/Pop';
 
 class ParticipantsService {
 
@@ -25,31 +24,29 @@ class ParticipantsService {
   }
 
   async submitAnswer(challengeId, participantId, submission){
-    const res = await api.post(`api/challenges/${challengeId}/participants/${participantId}`, {
-      challengeId: challengeId,
-      participantId: participantId,
-      answer: submission,
-    })
-    if(res.data.participant.status == 'incomplete'){
-      Pop.error("Answer was incorrect.")
-    }
-    if(res.data.participant.status == 'completed'){
-      Pop.success("Congratulations on finishing the challenge!")
-    }
+    throw new Error('Needs Moved to ChallengesService')
+    
+    // const res = await api.post(`api/challenges/${challengeId}/participants/${participantId}`, {
+    //   challengeId: challengeId,
+    //   participantId: participantId,
+    //   answer: submission,
+    // })
+    // if(res.data.participant.status == 'incomplete'){
+    //   Pop.error("Answer was incorrect.")
+    // }
+    // if(res.data.participant.status == 'completed'){
+    //   Pop.success("Congratulations on finishing the challenge!")
+    // }
   }
 
-  async updateChallengeParticipant(participantId, newSubmission) {
-    const res = await api.put(`api/participants/${participantId}`, newSubmission)
-    logger.log('Participant Updated ⏩', res.data)
-    AppState.activeParticipant = res.data
-    return res.data
-  }
 
   async gradeChallengeParticipant(newGrade) {
-    const res = await api.put(`api/moderators/${newGrade.participantId}/grade`, newGrade)
-    logger.log('Participant Updated ⏩', res.data)
-    AppState.activeParticipant = res.data
-    return res.data
+    throw new Error('Needs Moved to ChallengesService')
+    
+    // const res = await api.put(`api/moderators/${newGrade.participantId}/grade`, newGrade)
+    // logger.log('Participant Updated ⏩', res.data)
+    // AppState.activeParticipant = res.data
+    // return res.data
   }
 
   async leaveChallenge(participantId) {
