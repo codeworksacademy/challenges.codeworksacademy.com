6f10e7a1d7128a952e307d16301768736f4cd7ef
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2024-01-23 16:03:01 -0700

    Refactor ChallengePage.vue layout

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/ChallengePage.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/ChallengePage.vue
@@ -1,11 +1,11 @@
 <template>
-  <section v-if="challenge" :key="challenge?.id" class="container-fluid text-light pb-5">
+  <section v-if="challenge" :key="challenge?.id" class="text-light pb-5">
     <div class="col-12" :style="`background-image: url(${challenge.coverImg}); opacity: .9; background-repeat: no-repeat; background-size: cover; background-position:center; height:150px; overflow-x: hidden;`">
     <h1 class="m-5 text-center">{{ challenge.name }}</h1>
     </div>
     <div class="d-flex mobile-column-query justify-content-center pt-5" style="height: 100%; background: #161d2b">
       <!-- STUB - Offcanvas Challenge Detail router-view links -->
-      <div class="col-md-4 rounded-3 mobile-menu">
+      <div class="col-md-4 rounded-3 mobile-menu ps-3">
         <ChallengeDetailsMenu />
       </div>
       <!-- STUB - Challenge Details for active route -->
