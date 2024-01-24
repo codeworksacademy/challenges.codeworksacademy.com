5f0c54fd7f17b1ee1d4b8861e7663a08b5da5885
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2024-01-24 16:37:16 -0700

    Update AccountLinksCard and AccountPage styles

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/AccountAndProfilePage/AccountLinksCard.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/AccountAndProfilePage/AccountLinksCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-3 mx-1 px-3 py-2 user-links-card rounded d-flex flex-column">
+  <div class="my-3 ms-2 px-3 py-2 user-links-card rounded-3 d-flex flex-column">
     <p class="text-white-50">
       User Links
     </p>
--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/AccountPage.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/pages/AccountPage.vue
@@ -4,16 +4,14 @@
       <div class="p-0">
         <img class="coverImg-style" :src="account.coverImg" alt="Cover Image">
       </div>
-
-      <div class="col-12 text-white">
+      <div class="col-12 text-white mb-3">
         <section class="row justify-content-between">
           <div class="col-md-7 col-12 d-flex summary-height">
             <SummarySection :profile="account" :milestones="milestones" :challenges="challenges" :participation="participation" />
           </div>
-
           <div class="col-4 align-items-center justify-content-end d-none d-md-flex summary-height">
             <router-link :to="{ name: 'Account.challenges' }">
-              <button class="btn aqua-btn-outline my-2">
+              <button class="btn aqua-btn-outline my-2 me-3">
                 View my challenges
               </button>
             </router-link>
