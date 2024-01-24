9ef1714de1d28527fb58630155ad88cf3b63c96a
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-01-24 13:23:17 -0700

    fixed rep card

--- CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengePage/ChallengeReputationCard.vue
+++ CodeWorks_Challenges/CodeWorks_Challenges.client/src/components/ChallengePage/ChallengeReputationCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <div :class="`details-card ${themeStyle ? 'theme-style' : ''} rounded text-capitalize`" :style="{backgroundColor: bgColor, borderColor: color, borderStyle: 'groove'}">
-    <!-- <i class="mdi mdi-account-star-outline"></i> -->
-    <!-- <h3 class="circle-container"><span class="circle">+{{ challenge.reputationIds.length }}</span></h3> -->
-    <h3>Challenge Reputation {{ challenge.reputationIds.length }}</h3>
-    <h3>Creator Reputation {{ challenge.creator.reputation }}</h3>
-    <!-- <h6 class="text-center pb-2">Total Reputation</h6> -->
+  <div :class="`details-card d-flex justify-content-center align-items-center ${themeStyle ? 'theme-style' : ''} rounded text-capitalize pt-3`" :style="{backgroundColor: bgColor, borderColor: color, borderStyle: 'groove'}">
+    <i class="mdi mdi-account-star-outline"></i>
+    <h3 class="circle-container"><span class="circle">+{{ challenge.reputationIds.length }}</span></h3>
+    <div class="d-flex flex-column justify-content-center align-items-center text-white text-capitalize pb-5 pt-3">
+      <h3 class="text-capitalize">Reputation</h3>
+      <h6>Creator <span class="text-lowercase">(total)</span>: {{ challenge.creator.reputation }}</h6>
+      <h6>Challenge <span class="text-lowercase">(total)</span>: {{ challenge.reputationIds.length }}</h6>
+    </div>
   </div>
 </template>
 
@@ -26,7 +28,7 @@ export default {
 <style scoped lang="scss">
 .mdi-account-star-outline {
   position: relative;
-  top: 10%;
+  top: 20%;
   font-size: 3rem;
   transform: translateX(-10%);
 }
@@ -40,18 +42,18 @@ export default {
   .circle {
     position: absolute;
     top: 0;
-    right: 25%;
-    transform: translateY(-150%);
+    right: 36%;
+    transform: translateY(-85%);
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
     box-shadow: inset 0 0 0 2px #2a8f4c;
     border-radius: 50%;
     background-color: #38BB64;
     color: white;
-    font-size: 1.3rem;
+    font-size: 1.1rem;
     display: flex;
     justify-content: center;
     align-items: center;
