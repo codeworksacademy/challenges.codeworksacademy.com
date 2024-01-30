adcb2adaeded52513b6ea668192499ae6e9f6a82
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-01-30 14:10:07 -0700

    removed it from DbContext

--- CodeWorks_Challenges/CodeWorks_Challenges/src/models/EZCache.js
+++ CodeWorks_Challenges/CodeWorks_Challenges/src/models/EZCache.js
@@ -1,23 +1,4 @@
-// import { Schema } from "mongoose";
-// const ObjectId = Schema.Types.ObjectId
 
-// // I dont know if I need a model yet
-// export const CacheSchema = new Schema({
-//   cacheId: {
-//     type: String,
-//     required: true,
-//   },
-//   cachedData: {
-//     type: Array,
-//     default: []
-//   },
-//   lastEmptied: {
-//     type: Number,
-//     default: Date.now()
-//   }
-// },
-//   { timestamps: true, toJSON: { virtuals: true } }
-// )
 export class EZCache {
   constructor(data) {
     this.cacheId = data.cacheId
