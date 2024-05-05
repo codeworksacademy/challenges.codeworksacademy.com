1504ed0267f264fadb1a473a49d22e33c4b4ba47
Jake Overall <joverall22@gmail.com>
2024-05-04 18:28:01 -0600

    Update env.js

--- src/client/src/env.js
+++ src/client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-82py0l5jt61zn10o.us.auth0.com'
-export const clientId = 'hykoUyNDCrsovYQXXUKObg3y1Iw4UhUS' 
-export const audience = 'http://sandbox.com'
+export const domain = dev ? 'dev-82py0l5jt61zn10o.us.auth0.com' : 'codeworksacademy.auth0.com'
+export const clientId = dev ? 'hykoUyNDCrsovYQXXUKObg3y1Iw4UhUS' : 'Pr738Hn5ZZhYYahOhTukx3phzlIPGCfl'
+export const audience = dev ? 'http://sandbox.com' : 'https://codeworksacademy.com'
